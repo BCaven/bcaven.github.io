41b2ea97d48dc661b8da67c2b32f9be2d5788a73
BCaven <blakecaven@outlook.com>
2024-03-13 20:56:18 -0400

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <h1>Hello world</h1>
+  <v-app>
+    <v-main>
+      <h1>Hello world</h1>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
